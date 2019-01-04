d6868267dc1209047ddae992888dd9c832be196f
Josedn <joserafael161@hotmail.com>
2019-01-03 22:11:34 -0500

    Add react

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <canvas className="game">
+        </canvas>
+      </div>
+    );
+  }
+}
+
+export default App;
