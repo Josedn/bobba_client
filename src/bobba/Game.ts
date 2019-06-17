8985185b9f052dd4c1710c23288efa4a698deab6
Jose D <joserafael161@hotmail.com>
2019-06-17 09:38:37 -0500

    More minor fixes

--- src/bobba/Game.ts
+++ src/bobba/Game.ts
@@ -37,29 +37,27 @@ export default class Game {
         ];
 
         Promise.all([
-            this.avatarImager.initialize(),
+            this.avatarImager.initialize().then(() => this.ghostTextures.initialize()),
             this.furniImager.initialize(),
             this.engine.loadGlobalTextures(sprites)
         ]).then(() => {
-            this.ghostTextures.initialize().then(() => {
-                this.currentRoom = new Room(1, "Dummy room", RoomModel.getDummyRoomModel());
-                this.currentRoom.roomUserManager.addUserToRoom(1, 4, 8, 0, 0, "Relv", "hd-190-10.lg-3023-1408.ch-215-91.hr-893-45");
-                this.currentRoom.roomUserManager.addUserToRoom(2, 4, 10, 0, 4, "Grav", "ca-1811-62.lg-3018-81.hr-836-45.ch-669-1193.hd-600-10");
-                this.currentRoom.roomItemManager.addItemToRoom(256, 4, 4, 0, 2, 0, 267);
-                this.currentRoom.roomItemManager.addItemToRoom(257, 7, 6, 0, 2, 1, 1623);
-                this.currentRoom.roomItemManager.addItemToRoom(258, 7, 4, 0, 0, 1, 1565);
-    
-                this.currentRoom.roomItemManager.addItemToRoom(259, 0, 0, 0, 2, 0, 4651);
-                this.currentRoom.roomItemManager.addItemToRoom(260, 0, 1, 0, 2, 0, 4651);
-                this.currentRoom.roomItemManager.addItemToRoom(261, 0, 2, 0, 2, 0, 4651);
-    
-                this.currentRoom.roomItemManager.addItemToRoom(262, 0, 0, 0, 4, 0, 4651);
-                this.currentRoom.roomItemManager.addItemToRoom(263, 1, 0, 0, 4, 0, 4651);
-                this.currentRoom.roomItemManager.addItemToRoom(264, 2, 0, 0, 4, 0, 4651);
-    
-                this.currentRoom.roomItemManager.addItemToRoom(265, 0, 5, 0, 0, 0, 173);
-                
-            });
+            this.currentRoom = new Room(1, "Dummy room", RoomModel.getDummyRoomModel());
+            this.currentRoom.roomUserManager.addUserToRoom(1, 4, 8, 0, 0, "Relv", "hd-190-10.lg-3023-1408.ch-215-91.hr-893-45");
+            this.currentRoom.roomUserManager.addUserToRoom(2, 4, 10, 0, 4, "Grav", "ca-1811-62.lg-3018-81.hr-836-45.ch-669-1193.hd-600-10");
+            this.currentRoom.roomItemManager.addItemToRoom(256, 4, 4, 0, 2, 0, 267);
+            this.currentRoom.roomItemManager.addItemToRoom(257, 7, 6, 0, 2, 1, 1623);
+            this.currentRoom.roomItemManager.addItemToRoom(258, 7, 4, 0, 0, 1, 1565);
+
+            this.currentRoom.roomItemManager.addItemToRoom(259, 0, 0, 0, 2, 0, 4651);
+            this.currentRoom.roomItemManager.addItemToRoom(260, 0, 1, 0, 2, 0, 4651);
+            this.currentRoom.roomItemManager.addItemToRoom(261, 0, 2, 0, 2, 0, 4651);
+
+            this.currentRoom.roomItemManager.addItemToRoom(262, 0, 0, 0, 4, 0, 4651);
+            this.currentRoom.roomItemManager.addItemToRoom(263, 1, 0, 0, 4, 0, 4651);
+            this.currentRoom.roomItemManager.addItemToRoom(264, 2, 0, 0, 4, 0, 4651);
+
+            this.currentRoom.roomItemManager.addItemToRoom(265, 0, 5, 0, 0, 0, 173);
+
         });
     }
 
