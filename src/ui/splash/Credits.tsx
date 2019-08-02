8d243ac0f8365594a4a3dce4f7741db3229f736e
Jose D <joserafael161@hotmail.com>
2019-08-02 13:31:13 -0500

    Fix furni icons overflow

--- src/ui/splash/Credits.tsx
+++ src/ui/splash/Credits.tsx
@@ -5,7 +5,7 @@ const Credits = () => {
         <p className="main_content_footer">
             Habbo is a registered trademark of Sulake Oy. All rights reserved to their respective owners.
             <br />
-            Made by Relevance. Follow me on <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/josednn/">instagram</a>.
+            <span style={{fontFamily: "'Volter'", fontSize: '9px'}}>Made with | by Relevance. Follow me on <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/josednn/">instagram</a>.</span>
         </p>
     );
 };
