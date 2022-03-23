8e4e5524f0ece22f1a2725f8bb709d63642a4f2f
José D <joserafael161@hotmail.com>
2022-03-22 23:14:07 -0500

    Update Constants.ts

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,9 +1,9 @@
 export default class Constants {
     public static readonly WS_URL = "wss://game.bobba.io/"
 
-    public static readonly AVATAR_RESOURCES_URL = "//images.bobba.io/gordon/PRODUCTION-202006192205-424220153/";
-    public static readonly FURNI_RESOURCES_URL = "//images.bobba.io/dcr/hof_furni/";
-    public static readonly CATALOGUE_RESOURCES_URL = "//images.bobba.io/c_images/catalogue/";
-    public static readonly TOP_STORY_RESOURCES_URL = "//images.bobba.io/c_images/Top_Story_Images/";
-    public static readonly BADGE_RESOURCES_URL = "//images.bobba.io/c_images/album1584/";
+    public static readonly AVATAR_RESOURCES_URL = "https://images.bobba.io/gordon/PRODUCTION-202006192205-424220153/";
+    public static readonly FURNI_RESOURCES_URL = "https://images.bobba.io/dcr/hof_furni/";
+    public static readonly CATALOGUE_RESOURCES_URL = "https://images.bobba.io/c_images/catalogue/";
+    public static readonly TOP_STORY_RESOURCES_URL = "https://images.bobba.io/c_images/Top_Story_Images/";
+    public static readonly BADGE_RESOURCES_URL = "https://images.bobba.io/c_images/album1584/";
 }
